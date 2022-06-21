656321e27548bc412428a0f6b41bfd46fe1a459a
teabana <micky59322@gmail.com>
2022-06-22 00:27:54 +0900

    side_bar_fix

--- app/controllers/beach_controller.rb
+++ app/controllers/beach_controller.rb
@@ -3,6 +3,9 @@ class BeachController < ApplicationController
   def index
   end
 
+  def search_index
+  end
+
   def search
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'beach#index'
-  resources :beachs, only: :index
-  get 'beach/search', to: 'beach#search'
+  resources :beachs do
+    collection do
+      get 'serch'
+    end
+  end
+  get 'beach/search', to: 'beach#search_index'
 end
